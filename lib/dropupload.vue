abc7299418139c969f29f3fcc485cd4047766cde
杨玉峰 <1539558157@qq.com>
2019-01-25 10:16:03 +0800

    first commit

---
+++ lib/dropupload.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="dropzone" @click="checkFile" @drop="onDrag" @dragleave="stop" @ragover="stop" @dragenter="stop" @dragover="stop">
+    <input class="upload-yyf-file" type="file" :accept="accept" @change="$emit('getFile', $event)"/>
+    <slot class="slot"></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    accept: String
+  },
+  methods: {
+    stop (e) {
+      e.preventDefault()
+    },
+    onDrag (e) {
+      e.preventDefault()
+      this.$emit('getFile', e)
+    },
+    checkFile () {
+      this.$el.querySelectorAll('upload-yyf-file')[0].click()
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .dropzone {
+    position: relative;
+    height: 200px;
+    background-color: blue;
+  }
+  input {
+    position: absolute;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+  .slot {
+    pointer-events: none;
+  }
+</style>
---
+++ readme.md
@@ -0,0 +1,20 @@
+## 安装
+```
+npm i -save dragupload
+或
+yarn add dragupload
+```
+## 使用
+```
+<dragupload @getFile="upload" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"></dragupload>
+```
+## 参数
+
+参数 | 含义| 类型| 是否必须
+-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+accept|	允许上传的文件类型|	String|	true
+## 事件
+
+参数 | 含义 | 类型
+-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+getFile|	文件选择事件|	event
